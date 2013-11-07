5349e1fe82c8823c921bf942e82f13869a471ce4
Alexander Bürger <alexander.buerger@met.no>
2013-11-07 15:53:07 +0000

    miLogger/miLogging: METLIBS_LOG_SCOPE may have argument
     * if called with argument, the argument is passed to METLIBS_LOG_DEBUG

--- miLogging.h
+++ miLogging.h
@@ -118,16 +118,20 @@ namespace /* anonymous */ {
   ::milogger::detail::Category INTERNAL_milogger = ::milogger::detail::getCategory(MILOGGER_CATEGORY, __func__); \
   const ::milogger::detail::cls MILOGGER_scope(INTERNAL_milogger, __PRETTY_FUNCTION__)
 
-#define METLIBS_LOG_SCOPE()                     \
-  MILOGGER_DETAIL_LOG_SCOPE(LogScope)
+// modified (maybe wrongly) from http://gustedt.wordpress.com/2010/06/08/detect-empty-macro-arguments/
+#define MILOGGER_DETAIL_ARG3(arg1, arg2, arg3, ...) arg3
+#define MILOGGER_DETAIL_SWITCH_HAS_ARG(YES, NO, ...) MILOGGER_DETAIL_ARG3(__VA_ARGS__, NO, YES)
+#define MILOGGER_DETAIL_MAKE_COMMA(...) ,
+#define MILOGGER_DETAIL_CALL_IF_ARGS(CALL, ...)                        \
+  MILOGGER_DETAIL_SWITCH_HAS_ARG(CALL, , MILOGGER_DETAIL_MAKE_COMMA __VA_ARGS__ ())
 
-#ifdef M_TIME
-#define METLIBS_LOG_TIME()                      \
-  MILOGGER_DETAIL_LOG_SCOPE(LogTime)
-#else // M_TIME
-#define METLIBS_LOG_TIME()                      \
-  METLIBS_LOG_SCOPE()
-#endif // M_TIME
+#define METLIBS_LOG_SCOPE(...)                                          \
+  MILOGGER_DETAIL_LOG_SCOPE(LogScope)                                   \
+  MILOGGER_DETAIL_CALL_IF_ARGS(; METLIBS_LOG_DEBUG(__VA_ARGS__), __VA_ARGS__)
+
+#define METLIBS_LOG_TIME(...)                                           \
+  MILOGGER_DETAIL_LOG_SCOPE(LogTime)                                    \
+  MILOGGER_DETAIL_CALL_IF_ARGS(; METLIBS_LOG_DEBUG(__VA_ARGS__), __VA_ARGS__)
 
 #define LOGVAL(x) " " #x "='" << x << "'"
 
