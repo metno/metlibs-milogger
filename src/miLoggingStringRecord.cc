ea7e18546a1efad145dd138e4fbd7255a5912846
Alexander Bürger <alexander.buerger@met.no>
2017-04-06 08:00:08 +0200

    add option to disable use of boost pool allocator

--- src/miLoggingStringRecord.cc
+++ src/miLoggingStringRecord.cc
@@ -1,7 +1,9 @@
 
 #include "miLoggingStringRecord.h"
 
+#ifndef MILOGGER_DO_NOT_USE_POOL_ALLOCATOR
 #include <boost/pool/pool_alloc.hpp>
+#endif
 
 namespace milogger {
 namespace record {
@@ -17,15 +19,18 @@ StringRecord::StringRecord()
 }
 
 
+#ifndef MILOGGER_DO_NOT_USE_POOL_ALLOCATOR
 typedef boost::fast_pool_allocator<StringRecord> StringRecord_pool_t;
-
-StringRecord_pool_t poolStringRecord;
+static StringRecord_pool_t poolStringRecord;
+#endif
 
 StringRecordPtr allocateStringRecord(Severity s)
 {
   // TODO this is still allocating memory for the string "mText",
   // while it would only need to "clear" on an existing StringRecord
-#if 0
+#ifdef MILOGGER_DO_NOT_USE_POOL_ALLOCATOR
+  return std::make_shared<StringRecord>(s);
+#elif 0
   return std::allocate_shared<StringRecord, StringRecord_pool_t>(poolStringRecord, s);
 #else
   // boost 1.58 fast_pool_allocator seems to have problems with
